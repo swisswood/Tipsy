f79debe05808ff772c4660ea68356a33db6c2fc6
swisswood <swisswood222@gmail.com>
2022-09-27 02:04:37 +0900

    Edit

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,6 +9,8 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
+    var splitNumber: Int = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,9 +21,11 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = Int(sender.value).description
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
     }
     
 
