3b6ac9fdbdd444cb3e4576373dd3de8c16051429
swisswood <swisswood222@gmail.com>
2022-09-27 02:59:56 +0900

    Edit

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,15 +9,26 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    var splitNumber: Int = 2
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func tipChanged(_ sender: UIButton) {
-        zeroPctButton.isSelected = true
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        if sender.currentTitle == "0%" {
+            zeroPctButton.isSelected = true
+            billTextField.endEditing(true)
+        } else if sender.currentTitle == "10%" {
+            tenPctButton.isSelected = true
+            billTextField.endEditing(true)
+        } else if sender.currentTitle == "20%" {
+            twentyPctButton.isSelected = true
+            billTextField.endEditing(true)
+        }
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
@@ -25,6 +36,16 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        let bill = Float(billTextField.text!)
+       
+        if zeroPctButton.isSelected == true {
+            print(bill! * 1.0 / Float(splitNumberLabel.text!)!)
+        } else if tenPctButton.isSelected == true {
+            print(bill! * 1.1 / Float(splitNumberLabel.text!)!)
+        } else {
+            print(bill! * 1.2 / Float(splitNumberLabel.text!)!)
+        }
+        
         
     }
     
